1d323aeb8588e97e1d0ae88fcdea69f910460c8f
romanstrazanec <roman.strazanec007@gmail.com>
2018-05-05 19:21:59 +0200

    add gameactivity

---
+++ app/src/main/java/romanstrazanec/minesweeper/GameActivity.java
@@ -0,0 +1,33 @@
+package romanstrazanec.minesweeper;
+
+import android.os.Bundle;
+import android.os.Handler;
+import android.os.Message;
+import android.support.v7.app.AppCompatActivity;
+
+public class GameActivity extends AppCompatActivity {
+    UpdateThread updateThread;
+    Handler updateHandler;
+    GameCanvas gameCanvas;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        gameCanvas = new GameCanvas(this);
+        setContentView(gameCanvas);
+        createHandler();
+        updateThread = new UpdateThread(updateHandler);
+        updateThread.start();
+    }
+
+    private void createHandler() {
+        updateHandler = new Handler() {
+            public void handleMessage(Message msg) {
+                gameCanvas.update();
+                gameCanvas.invalidate();
+                super.handleMessage(msg);
+            }
+        };
+    }
+}
