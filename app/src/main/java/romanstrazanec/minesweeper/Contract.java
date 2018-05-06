955301b0f1e9da0c22a263432b9b3365c4e467d1
romanstrazanec <roman.strazanec007@gmail.com>
2018-05-06 17:03:10 +0200

    contract.settings

---
+++ app/src/main/java/romanstrazanec/minesweeper/Contract.java
@@ -0,0 +1,13 @@
+package romanstrazanec.minesweeper;
+
+public class Contract {
+
+    public class Settings {
+        public static final String TABLE_NAME = "Settings";
+        public static final String COLUMN_WIDTH = "width";
+        public static final String COLUMN_HEIGHT = "height";
+        public static final String COLUMN_MINES = "mines";
+        public static final String COLUMN_COLOR = "color";
+        public static final String COLUMN_COLORFUL_NUMBERS = "colorful_numbers";
+    }
+}
