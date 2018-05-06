d3a14a5cefefa97b378c0bcdbd250092889b7cf2
romanstrazanec <roman.strazanec007@gmail.com>
2018-05-06 17:06:24 +0200

    Settings class

---
+++ app/src/main/java/romanstrazanec/minesweeper/Settings.java
@@ -0,0 +1,53 @@
+package romanstrazanec.minesweeper;
+
+public class Settings {
+    private int width, height, mines, color, colorfulnumbers;
+
+    public Settings(int width, int height, int mines, int color, int colorfulnumbers) {
+        this.width = width;
+        this.height = height;
+        this.mines = mines;
+        this.color = color;
+        this.colorfulnumbers = colorfulnumbers;
+    }
+
+    public int getWidth() {
+        return width;
+    }
+
+    public void setWidth(int width) {
+        this.width = width;
+    }
+
+    public int getHeight() {
+        return height;
+    }
+
+    public void setHeight(int height) {
+        this.height = height;
+    }
+
+    public int getMines() {
+        return mines;
+    }
+
+    public void setMines(int mines) {
+        this.mines = mines;
+    }
+
+    public int getColor() {
+        return color;
+    }
+
+    public void setColor(int color) {
+        this.color = color;
+    }
+
+    public int getColorfulnumbers() {
+        return colorfulnumbers;
+    }
+
+    public void setColorfulnumbers(int colorfulnumbers) {
+        this.colorfulnumbers = colorfulnumbers;
+    }
+}
