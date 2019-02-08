4764fdbfd9ae571c3f934928421e8449b57b9e66
romanstrazanec <roman.strazanec007@gmail.com>
2019-02-08 18:27:24 +0100

    modify counter

--- src/Counter.java
+++ src/Counter.java
@@ -3,28 +3,20 @@ import java.awt.Color;
 import java.awt.Graphics;
 import javax.swing.JComponent;
 
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-
-/**
- *
- * @author NAY
- */
 public class Counter extends JComponent {
     private int number;
+
     public Counter(int number, int x, int y) {
         this.number = number;
-        setBounds(x,y,30,10);
+        setBounds(x, y, 30, 10);
     }
+
     @Override
     public void paintComponent(Graphics g){
         super.paintComponent(g);
         g.setColor(Color.black);
-        g.drawString(""+number, 1, 10);
-        g.drawString(""+number, 2, 10);
+        g.drawString("" + number, 1, 10);
+        g.drawString("" + number, 2, 10);
     }
 
     public int getNumber() {
@@ -35,5 +27,4 @@ public class Counter extends JComponent {
         this.number = number;
         repaint();
     }
-    
 }
