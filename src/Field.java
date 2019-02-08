6e76ae0a983c1d524cc5e0f7ecb6a56981d3dddb
romanstrazanec <roman.strazanec007@gmail.com>
2019-02-08 19:45:53 +0100

    reformat

--- src/Field.java
+++ src/Field.java
@@ -8,25 +8,13 @@ import java.io.IOException;
 import javax.swing.JComponent;
 import javax.swing.JFrame;
 
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-
-/**
- *
- * @author NAY
- */
-
 public class Field extends JComponent {
     private int width, height, bombs, flags, widthoftile, opened;
     private boolean firstClick, dead, colorfulnumbers;
     private Tile[][] tiles;
     private Color cl;
-    //konštruktor
+    
     public Field(JFrame jfr, int x, int y){
-        //načíta sa šírka, výška, počet bômb, farba, farebné čísla zo súboru 
         try (BufferedReader file = new BufferedReader(new FileReader("settings.dat"))){
             String[] row = file.readLine().split(";");
             width = Integer.parseInt(row[1]);
@@ -53,119 +41,96 @@ public class Field extends JComponent {
             } catch (IOException ex) {}
         }
         flags = 0;
-        opened=0;
+        opened = 0;
         firstClick = false;
         makeField(width, height, bombs);
-        setBounds(x, y, width*widthoftile, height*widthoftile);
-        jfr.setSize(this.getWidth()+80, this.getHeight()+120);
+        setBounds(x, y, width * widthoftile, height * widthoftile);
+        jfr.setSize(width + 80, height + 120);
     }
-    //výroba poľa
+    
     private void makeField(int width, int height, int bombs){
         tiles = new Tile[width][height];
-        for (int j = 0; j < height; j++) {
-            for (int i = 0; i < width; i++) {
-                Tile t = new Tile(30 * i, 30*j, 30, cl, colorfulnumbers);
-                tiles[i][j]=t;
+        for (int j = 0; j < height; ++j)
+            for (int i = 0; i < width; ++i) {
+                Tile t = new Tile(30 * i, 30 * j, 30, cl, colorfulnumbers);
+                tiles[i][j] = t;
                 add(t);
                 widthoftile = t.getWidth();
             }
-        }
         placeBombs(bombs); 
         setNumbers();
         repaint();
     }
-    //nastavenie bômb
+    
     private void placeBombs(int number_of_bombs){
-        for(int i=0;i<number_of_bombs;i++){
-            placeBomb();
-        }
+        for(int i = 0; i < number_of_bombs; ++i) placeBomb();
     }
-    //nastaví bombu na náhodnú pozíciu
+    
     private void placeBomb(){
-        int x = (int)(Math.random()*width); 
-        int y = (int)(Math.random()*height);
+        int x = (int)(Math.random() * width); 
+        int y = (int)(Math.random() * height);
         if(!tiles[x][y].isBomb()) tiles[x][y].setBomb(true);
         else placeBomb();
     }
+    
     public void replaceBomb(int x, int y){
-        if (x<width && y < height) {
-            if (tiles[x][y].isBomb()) {
-                placeBomb();
-            }
+        if(x < width && y < height) {
+            if(tiles[x][y].isBomb()) placeBomb();
             tiles[x][y].setBomb(false);
             setNumbers();
         }
-    } //bomba by nemala nikdy byť prva
-    //otvorí pole; ak má pole nulovu hodnotu počtu bômb okolo, rekurziou sa otvárajú ďalšie polia, hranicou sú polia s hodnotou počtu bômb okolo vačšou ako 1 alebo okraj plochy
+    }
+    
     private void openTiles(int x, int y){
         tiles[x][y].openTile();
-        opened++;
-        if(tiles[x][y].getNumberofNearBombs()==0){
-            if( x>0        && y>0        && !tiles[x-1][y-1].isFlag() && !tiles[x-1][y-1].isOpen() && tiles[x-1][y-1].getNumberofNearBombs()>=0) openTiles(x-1,y-1);
-            if(               y>0        && !tiles[x][y-1].isFlag()   && !tiles[x][y-1].isOpen()   && tiles[x][y-1].getNumberofNearBombs()>=0)   openTiles(x,y-1);
-            if( x<width-1  && y>0        && !tiles[x+1][y-1].isFlag() && !tiles[x+1][y-1].isOpen() && tiles[x+1][y-1].getNumberofNearBombs()>=0) openTiles(x+1,y-1);
-            if( x>0                      && !tiles[x-1][y].isFlag()   && !tiles[x-1][y].isOpen()   && tiles[x-1][y].getNumberofNearBombs()>=0)   openTiles(x-1,y);
-            if( x<width-1                && !tiles[x+1][y].isFlag()   && !tiles[x+1][y].isOpen()   && tiles[x+1][y].getNumberofNearBombs()>=0)   openTiles(x+1,y);
-            if( x>0        && y<height-1 && !tiles[x-1][y+1].isFlag() && !tiles[x-1][y+1].isOpen() && tiles[x-1][y+1].getNumberofNearBombs()>=0) openTiles(x-1,y+1);
-            if(               y<height-1 && !tiles[x][y+1].isFlag()   && !tiles[x][y+1].isOpen()   && tiles[x][y+1].getNumberofNearBombs()>=0)   openTiles(x,y+1);
-            if( x<width-1  && y<height-1 && !tiles[x+1][y+1].isFlag() && !tiles[x+1][y+1].isOpen() && tiles[x+1][y+1].getNumberofNearBombs()>=0) openTiles(x+1,y+1);
+        ++opened;
+        if(tiles[x][y].getNumberofNearBombs() == 0){
+            if(x > 0 && y > 0 && !tiles[x - 1][y - 1].isFlag() && !tiles[x - 1][y - 1].isOpen() && tiles[x - 1][y - 1].getNumberofNearBombs() >= 0) openTiles(x - 1,y - 1);
+            if(y > 0 && !tiles[x][y - 1].isFlag() && !tiles[x][y - 1].isOpen() && tiles[x][y - 1].getNumberofNearBombs() >= 0) openTiles(x,y - 1);
+            if(x < width - 1  && y > 0 && !tiles[x + 1][y - 1].isFlag() && !tiles[x + 1][y - 1].isOpen() && tiles[x + 1][y - 1].getNumberofNearBombs() >= 0) openTiles(x + 1,y - 1);
+            if(x > 0 && !tiles[x - 1][y].isFlag()   && !tiles[x - 1][y].isOpen() && tiles[x - 1][y].getNumberofNearBombs() >= 0) openTiles(x - 1,y);
+            if(x < width - 1 && !tiles[x + 1][y].isFlag()   && !tiles[x + 1][y].isOpen() && tiles[x + 1][y].getNumberofNearBombs() >= 0) openTiles(x + 1,y);
+            if(x > 0 && y < height - 1 && !tiles[x - 1][y + 1].isFlag() && !tiles[x - 1][y + 1].isOpen() && tiles[x - 1][y + 1].getNumberofNearBombs() >= 0) openTiles(x - 1,y + 1);
+            if( y < height - 1 && !tiles[x][y + 1].isFlag() && !tiles[x][y + 1].isOpen() && tiles[x][y + 1].getNumberofNearBombs() >= 0) openTiles(x,y + 1);
+            if(x < width - 1 && y < height - 1 && !tiles[x + 1][y + 1].isFlag() && !tiles[x + 1][y + 1].isOpen() && tiles[x + 1][y + 1].getNumberofNearBombs() >= 0) openTiles(x + 1,y + 1);
         }
     }
-    //nastaví každému poľu hodnotu počtu bômb okolo
+    
     private void setNumbers(){
-        for(int x=0; x<width; x++){
-            for(int y=0; y<height; y++){
-                if(!tiles[x][y].isBomb()) tiles[x][y].setNumberofNearBombs(getNumberofNearBombs(x, y));
-            }
-        }
+        for(int x = 0; x < width; ++x) for(int y = 0; y < height; ++y)
+            if(!tiles[x][y].isBomb()) tiles[x][y].setNumberofNearBombs(getNumberofNearBombs(x, y));
     }
-    //zistenie počtu bômb okolo
+    
     private int getNumberofNearBombs(int x, int y){
         int result = 0;
-        if(x>0        && y>0        && tiles[x-1][y-1].isBomb()) result++;
-        if(              y>0        && tiles[x][y-1].isBomb())   result++;
-        if(x<width-1  && y>0        && tiles[x+1][y-1].isBomb()) result++;
-        if(x>0                      && tiles[x-1][y].isBomb())   result++;
-        if(x<width-1                && tiles[x+1][y].isBomb())   result++;
-        if(x>0        && y<height-1 && tiles[x-1][y+1].isBomb()) result++;
-        if(              y<height-1 && tiles[x][y+1].isBomb())   result++;
-        if(x<width-1  && y<height-1 && tiles[x+1][y+1].isBomb()) result++;
+        if(x > 0 && y > 0 && tiles[x - 1][y - 1].isBomb()) ++result;
+        if(y > 0 && tiles[x][y - 1].isBomb()) ++result;
+        if(x < width - 1 && y > 0 && tiles[x + 1][y - 1].isBomb()) ++result;
+        if(x > 0 && tiles[x - 1][y].isBomb()) ++result;
+        if(x < width - 1 && tiles[x + 1][y].isBomb()) ++result;
+        if(x > 0 && y < height - 1 && tiles[x - 1][y + 1].isBomb()) ++result;
+        if(y < height - 1 && tiles[x][y + 1].isBomb()) ++result;
+        if(x < width - 1 && y < height - 1 && tiles[x + 1][y + 1].isBomb()) ++result;
         return result;
     }
-    //umiestni vlajku alebo ju odstráni, podľa toho pridá alebo odoberie z premenej flags
+    
     private void placeFlag(int x, int y){
-        if(tiles[x][y].isFlag()) flags--;
-        else flags++;
+        if(tiles[x][y].isFlag()) --flags;
+        else ++flags;
         tiles[x][y].setFlag(!tiles[x][y].isFlag());
     }
-    //otvorí všetky polia pri skončení hry
+    
     public void openAllTiles(){
-        for(int i=0; i<height; i++){
-            for(int j=0; j<width;j++){
-                if(!tiles[j][i].isOpen() && tiles[j][i].isBomb()) {
-                    tiles[j][i].openTile();
-                }
-            }
-        }
-        /*for(int i=0; i<height; i++){
-            for(int j=0; j<width;j++){
-                if(!tiles[j][i].isOpen()) {
-                    tiles[j][i].openTile();
-                }
-            }
-        }*/
+        for(int i=0; i < height; ++i) for(int j=0; j < width; ++j)
+            if(!tiles[j][i].isOpen() && tiles[j][i].isBomb()) tiles[j][i].openTile();
     }
-    //kliknutie myšou, x,y sú súradníce
+    
     public void leftClick(int x, int y){
         x = (x)/(widthoftile);
-        y = (y-5)/(widthoftile);
-        if (!firstClick && tiles[x][y].isBomb()) {
-            replaceBomb(x, y);
-        }
-        if (!firstClick) {
-            firstClick = true;
-        }
-        if(x<width && y<height && !tiles[x][y].isFlag() && !tiles[x][y].isOpen()){
+        y = (y - 5)/(widthoftile);
+        if(!firstClick && tiles[x][y].isBomb()) replaceBomb(x, y);
+        if(!firstClick) firstClick = true;
+        if(x < width && y < height && !tiles[x][y].isFlag() && !tiles[x][y].isOpen()){
             if(!tiles[x][y].isBomb()) openTiles(x, y);
             else {
                 tiles[x][y].setStav(4);
@@ -174,53 +139,33 @@ public class Field extends JComponent {
             }
         }
     }
+
     public void rightClick(int x, int y){
         x = (x)/(widthoftile);
-        y = (y-5)/(widthoftile);
-        if(x<width && y<height){
-            placeFlag(x, y);
-        }
+        y = (y - 5)/(widthoftile);
+        if(x < width && y < height) placeFlag(x, y);
     }
+
     public void mousePress(int x, int y, int oldx, int oldy){
         x = (x)/(widthoftile);
-        y = (y-5)/(widthoftile);
+        y = (y - 5)/(widthoftile);
         oldx = oldx/widthoftile;
-        oldy = (oldy-5)/widthoftile;
-        if (!tiles[oldx][oldy].isOpen() && !tiles[oldx][oldy].isFlag()) {
-            tiles[oldx][oldy].setStav(0);
-        }
-        if (!tiles[x][y].isOpen() && !tiles[x][y].isFlag()) {
-            tiles[x][y].setStav(1);
-        }
+        oldy = (oldy - 5)/widthoftile;
+        if(!tiles[oldx][oldy].isOpen() && !tiles[oldx][oldy].isFlag()) tiles[oldx][oldy].setStav(0);
+        if(!tiles[x][y].isOpen() && !tiles[x][y].isFlag()) tiles[x][y].setStav(1);
     }
-    //gettery & settery
-    public int getWidthofField() {
-        return width;
-    }
-    public int getHeightofField() {
-        return height;
-    }
-    public int getBombs() {
-        return bombs;
-    }
-    public int getWidthofTile() {
-        return widthoftile;
-    }
-    public boolean getFirstClick(){
-        return firstClick;
-    }
-    public int getFlags(){
-        return flags;
-    }
+    
+    public int getWidthofField() { return width; }
+    public int getHeightofField() { return height; }
+    public int getBombs() { return bombs; }
+    public int getWidthofTile() { return widthoftile; }
+    public boolean getFirstClick(){ return firstClick; }
+    public int getFlags(){ return flags; }
     public boolean isFlag(int x, int y){
-        x = (x)/(widthoftile);
-        y = (y-5)/(widthoftile);
+        x /= widthoftile;
+        y = (y - 5)/(widthoftile);
         return tiles[x][y].isFlag();
     }
-    public boolean getDead(){
-        return dead;
-    }
-    public int getOpened(){
-        return opened;
-    }
+    public boolean getDead(){ return dead; }
+    public int getOpened(){ return opened; }
 }
--- src/Tile.java
+++ src/Tile.java
@@ -3,72 +3,63 @@ import java.awt.Color;
 import java.awt.Graphics;
 import javax.swing.JComponent;
 
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-
-/**
- *
- * @author NAY
- */
 public class Tile extends JComponent {
-    private int stav, numberofnearbombs;
+    private int status, nbombsaround;
     private boolean bomb, flag, colorfulnumbers;
     private Color cl;
-    //konštruktory
+    
     public Tile(int x, int y, int width, Color cl, boolean colorfulnumbers){
-        numberofnearbombs = 0;
-        stav = 0;
+        nbombsaround = 0;
+        status = 0;
         bomb = false;
         this.flag = false;
         this.cl = cl;
         this.colorfulnumbers = colorfulnumbers;
         setBounds(x, y, width, width);
     }
-    
-    //grafika
+
     private void paintTile(Graphics g, Color c){
         g.setColor(c);
         g.fillRect(0, 0, getWidth(), getHeight());
         g.setColor(Color.black);
         g.drawRect(0, 0, getWidth(), getHeight());
     }
+
     private void paintSelectedTile(Graphics g){
-        if(cl==Color.blue) paintTile(g, Color.cyan);
-        if(cl==Color.green) paintTile(g, Color.yellow);
-        if(cl==Color.yellow) paintTile(g, Color.white);
-        if(cl==Color.red) paintTile(g, Color.magenta);
+        if(cl == Color.blue) paintTile(g, Color.cyan);
+        if(cl == Color.green) paintTile(g, Color.yellow);
+        if(cl == Color.yellow) paintTile(g, Color.white);
+        if(cl == Color.red) paintTile(g, Color.magenta);
     }
+
     private void paintBomb(Graphics g, Color c){
-        
         paintTile(g, c);
         g.setColor(Color.darkGray);
-        g.fillOval(3, 3, getWidth()-6, getHeight()-6);
+        g.fillOval(3, 3, getWidth() - 6, getHeight() - 6);
     }
     private void paintBlownBomb(Graphics g){
-        if(cl == Color.red) paintBomb(g,Color.yellow);
-        else paintBomb(g,Color.red);
+        if(cl == Color.red) paintBomb(g, Color.yellow);
+        else paintBomb(g, Color.red);
     }
     private void paintFlag(Graphics g){
-        if(cl==Color.blue) paintTile(g, Color.gray);
-        if(cl==Color.green) paintTile(g, Color.gray);
-        if(cl==Color.yellow) paintTile(g, new Color(200,150,0));
-        if(cl==Color.red) paintTile(g, Color.pink);
-        if (cl == Color.red) g.setColor(Color.yellow);
+        if(cl == Color.blue) paintTile(g, Color.gray);
+        if(cl == Color.green) paintTile(g, Color.gray);
+        if(cl == Color.yellow) paintTile(g, new Color(200,150,0));
+        if(cl == Color.red) paintTile(g, Color.pink);
+        if(cl == Color.red) g.setColor(Color.yellow);
         else g.setColor(Color.red);
-        g.fillRect(3, 3, getWidth()-6, getHeight()/2-6);
+        g.fillRect(3, 3, getWidth() - 6, getHeight()/2 - 6);
         g.setColor(Color.white);
-        g.drawLine(getWidth()-3, 3, getWidth()-3, getHeight()-3);
+        g.drawLine(getWidth() - 3, 3, getWidth() - 3, getHeight() - 3);
     }
+
     private void paintOpenTile(Graphics g){
-        if(cl==Color.blue) paintTile(g, Color.gray);
-        if(cl==Color.green) paintTile(g, Color.gray);
-        if(cl==Color.yellow) paintTile(g, new Color(200,150,0));
-        if(cl==Color.red) paintTile(g, Color.pink);
+        if(cl == Color.blue) paintTile(g, Color.gray);
+        if(cl == Color.green) paintTile(g, Color.gray);
+        if(cl == Color.yellow) paintTile(g, new Color(200,150,0));
+        if(cl == Color.red) paintTile(g, Color.pink);
         if(colorfulnumbers){
-            switch(numberofnearbombs){
+            switch(nbombsaround){
                 case 1: g.setColor(Color.BLUE);break;
                 case 2: g.setColor(Color.GREEN);break;
                 case 3: g.setColor(Color.RED);break;
@@ -80,72 +71,58 @@ public class Tile extends JComponent {
                 default: break;
             }
         } else g.setColor(Color.BLACK);
-        if(numberofnearbombs!=0) {
-            g.drawString(""+numberofnearbombs, 12, 20);
-            g.drawString(""+numberofnearbombs, 13, 20);
+        if(nbombsaround != 0) {
+            g.drawString("" + nbombsaround, 12, 20);
+            g.drawString("" + nbombsaround, 13, 20);
         }
     }
+
     @Override
     public void paintComponent(Graphics g){
         super.paintComponent(g);
-        switch(stav){
-            case 0: paintTile(g, cl); break; //skryte pole
-            case 1: paintSelectedTile(g); break;     //vybrané pole
-            case 2: paintOpenTile(g); break;         //otvorené pole
-            case 3: if(cl==Color.blue) paintBomb(g, Color.gray);
-                    if(cl==Color.green) paintBomb(g, Color.gray);
-                    if(cl==Color.yellow) paintBomb(g, new Color(200,150,0));
-                    if(cl==Color.red) paintBomb(g, Color.pink); break; //bomba
-            case 4: paintBlownBomb(g); break;        //vybuchnuta bomba
-            case 5: paintFlag(g); break; //vlajka   
+        switch(status){
+            case 0: paintTile(g, cl); break;
+            case 1: paintSelectedTile(g); break;
+            case 2: paintOpenTile(g); break;
+            case 3: if(cl == Color.blue) paintBomb(g, Color.gray);
+                    if(cl == Color.green) paintBomb(g, Color.gray);
+                    if(cl == Color.yellow) paintBomb(g, new Color(200,150,0));
+                    if(cl == Color.red) paintBomb(g, Color.pink); break;
+            case 4: paintBlownBomb(g); break;
+            case 5: paintFlag(g); break;
             default: break;
         }
     }
-    
-    //settery & gettery
-    public boolean isBomb(){
-        return bomb;
-    }
-    public boolean isFlag(){
-        return flag;
-    }
-    public boolean isOpen(){
-        return (stav==2 || stav==4);
-    }
-    public void setBomb(boolean bomb){
-        this.bomb = bomb;
-    } 
+
+    public boolean isBomb(){ return bomb; }
+    public boolean isFlag(){ return flag; }
+    public boolean isOpen(){ return (status==2 || status==4); }
+    public void setBomb(boolean bomb){ this.bomb = bomb; }
     public void setFlag(boolean flag){
         if (!isOpen()) {
             this.flag = flag;
-            stav = (flag) ? 5 : 0;
+            status = (flag) ? 5 : 0;
             repaint();
         }
     }
-    public int getStav(){
-        return stav;
-    }
-    public void setStav(int stav){
-        this.stav = stav;
+    public int getstatus(){ return status; }
+    public void setstatus(int status){
+        this.status = status;
         repaint();
     }
     public void openTile(){
-        if(bomb) stav=3;
-        else stav=2;
+        if(bomb) status = 3;
+        else status = 2;
         repaint();
     }
     public void selectTile(boolean select){
-        stav = (select) ? 1 : 0;
+        status = (select) ? 1 : 0;
         repaint();
     }
-    public void setNumberofNearBombs(int amount){
-        numberofnearbombs=amount;
-    }
-    public int getNumberofNearBombs(){
-        return numberofnearbombs;
-    }
+    public void setnbombsaround(int amount){ nbombsaround = amount; }
+    public int getnbombsaround(){ return nbombsaround; }
     public void setWidth(int width){
-        setBounds(0,0,width,width);
+        setBounds(0, 0, width, width);
         repaint();
     }
     public void setColor(Color cl){
